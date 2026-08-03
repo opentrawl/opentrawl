fe6bb6f0bc31cccfad947eec3578ae37fda2503f
joshp123 <joshp123@users.noreply.github.com>
2026-08-03 17:08:34 +0200

    Let Photos debug run production nodes

--- trawlers/photos/crawler.go
+++ trawlers/photos/crawler.go
@@ -131,7 +131,7 @@ func (c *Crawler) TrawlerCommands() []trawlkit.TrawlerCommand {
 			TrawlerCommandName:                    "debug",
 			TrawlerCommandHelpDescription:         "Inspect one production Photos operation",
 			TrawlerCommandPositionalArgumentNames: []string{"[NODE]", "[PHOTO]"},
-			TrawlerCommandChangesArchive:          false,
+			TrawlerCommandChangesArchive:          true,
 			TrawlerCommandArchiveAccess:           trawlkit.TrawlerCommandArchiveAccessRequired,
 			TrawlerCommandDiscoveryPlacement:      trawlkit.TrawlerCommandShownOnlyInTrawlerNamespaceHelp,
 			ExecuteTrawlerCommand:                 c.debugProductionNode,
